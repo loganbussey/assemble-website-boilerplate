df580b1e6961496fbb585c71ab137d4873dc2d02
Luke Bussey <luke.bussey@gmail.com>
2015-09-04 20:45:28 -0400

    Installed jit-grunt and time-grunt.
    Before:
    Execution Time (2015-09-05 00:41:02 UTC)
    loading tasks       1.9s  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 23%
    assemble:pages     502ms  ▇▇▇▇▇▇▇▇▇▇ 6%
    wiredep:app        124ms  ▇▇▇ 1%
    concurrent:dist       3s  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 35%
    autoprefixer:dist  328ms  ▇▇▇▇▇▇▇ 4%
    cssmin:generated   272ms  ▇▇▇▇▇▇ 3%
    uglify:generated    2.1s  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 26%
    Total 8.4s
    
    After:
    Execution Time (2015-09-05 00:42:48 UTC)
    loading tasks                  84ms  ▇▇▇ 2%
    loading assemble              164ms  ▇▇▇▇▇ 3%
    assemble:pages                675ms  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 12%
    wiredep:app                   195ms  ▇▇▇▇▇▇ 4%
    concurrent:dist                1.1s  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 20%
    loading grunt-autoprefixer    171ms  ▇▇▇▇▇ 3%
    autoprefixer:dist             301ms  ▇▇▇▇▇▇▇▇ 5%
    loading grunt-contrib-cssmin   80ms  ▇▇▇ 1%
    cssmin:generated              324ms  ▇▇▇▇▇▇▇▇▇ 6%
    loading grunt-contrib-uglify   90ms  ▇▇▇ 2%
    uglify:generated               2.1s  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 38%
    Total 5.6s

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@
 
 module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt, {
-        pattern: [
-            'assemble',
-            'grunt-*'
-        ]
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required grunt tasks
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
     });
 
     // configurable paths
