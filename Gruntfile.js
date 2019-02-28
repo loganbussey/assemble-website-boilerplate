3c45a4fdaea8cb857004972f6f609c438052c962
Luke Bussey <luke@loganbussey.com>
2019-02-27 18:06:49 -0600

    Fix local dev server autoprefixer to use .browserlistrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,20 +218,7 @@ module.exports = function (grunt) {
         },
         processors: [
           require('postcss-flexbugs-fixes'),
-          require('autoprefixer')({
-            browsers: [
-              'last 1 major version',
-              '>= 1%',
-              'Chrome >= 45',
-              'Firefox >= 38',
-              'Edge >= 12',
-              'Explorer >= 10',
-              'iOS >= 9',
-              'Safari >= 9',
-              'Android >= 4.4',
-              'Opera >= 30'
-            ]
-          })
+          require('autoprefixer')
         ]
       },
       server: {
